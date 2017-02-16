99515a82fc69f8a953a6b140c5471a0866e07ea2
ryujimano <ryujimano@gmail.com>
2017-02-15 23:32:31 -0500

    add tableview

---
+++ Yelp/BusinessTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BusinessTableViewCell.swift
+//  Yelp
+//
+//  Created by Ryuji Mano on 2/15/17.
+//  Copyright © 2017 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
